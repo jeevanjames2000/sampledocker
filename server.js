de14cac5dfdf160571d673b9fc4aea33933a74e8
jeevanjames2000 <jeevanjames2000@gmail.com>
2025-02-18 15:23:22 +0530

    sd

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ app.get("/", (req, res) => {
 app.get("/api", (req, res) => {
   res.json({ name: "jeevan" });
 });
-const PORT = 7000;
+const PORT = 7005;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
